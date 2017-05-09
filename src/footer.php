1f55f3b3c40f66a7978e8425649f6f258eb49fe3
Dani <info@elbit.cat>
2017-05-09 16:17:42 +0200

    cleanig wp author

--- src/footer.php
+++ src/footer.php
@@ -3,8 +3,8 @@
 
 				<!-- copyright -->
 				<p class="copyright">
-					&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>. <?php _e('Powered by', 'html5blank'); ?>
-					<a href="//wordpress.org">WordPress</a> &amp; <a href="//html5blank.com">HTML5 Blank</a>.
+					&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>. 
+					</a>.
 				</p>
 				<!-- /copyright -->
 
