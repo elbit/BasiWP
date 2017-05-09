4ad197b8be550a931c82caf56cc470d36b31c9f8
Dani <info@elbit.cat>
2017-05-09 16:19:14 +0200

    cleaned - conflict 2 jQ libraries

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,8 +1,9 @@
-(function( root, $, undefined ) {
-	"use strict";
-
-	$(function () {
-		// DOM ready, take it away
-	});
-
-} ( this, jQuery ));
+$(function() {
+    $(".ui.dropdown").dropdown();
+    $(".ui.menu-sticky").sticky();
+    $(".message .close").on("click", function() {
+            $(this)
+                .closest(".message")
+                .transition("fade");
+        });
+});
